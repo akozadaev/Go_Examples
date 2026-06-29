18ff8679804c0332c8ea272322f4dcfeab948452
Alexey Kozadaev <alexey.kozadaev@softezza.ru>
2026-06-30 00:35:23 +0300

    fix

---
+++ 2_if_else_example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func f(name string, result bool) bool {
+	fmt.Println(name)
+	return result
+}
+
+func main() {
+	if (f("A", false) && f("B", true)) || f("C", true) {
+		fmt.Println("ok")
+	}
+}
