8add682f0d05ef0fb808eaefb87f79e5e0371b6a
Alexey Kozadaev <alexey.kozadaev@softezza.ru>
2026-07-20 14:23:53 +0300

    добавляет тесты

--- 07_10_core_go/10_testing/calc_test.go
+++ 07_10_core_go/10_testing/calc_test.go
@@ -33,6 +33,7 @@ func TestWordCount(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			got := WordCount(tt.text)
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Fatalf("WordCount() = %#v, ожидается %#v", got, tt.want)
