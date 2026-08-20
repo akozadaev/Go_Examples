7bfff8c9ffc3fa8ec96181f17b9fc904c17bfe56
Alexey Kozadaev <alexey.kozadaev@softezza.ru>
2026-08-20 14:59:27 +0300

    исправляет тест примера пайплайна

--- 22_fanIn_fanOut/3_pipelines/main_test.go
+++ 22_fanIn_fanOut/3_pipelines/main_test.go
@@ -12,6 +12,9 @@ func TestPipeline(t *testing.T) {
 		got = append(got, value)
 	}
 	want := []int{16, 81}
+	if len(got) != len(want) {
+		t.Fatalf("got %v, want %v", got, want)
+	}
 	for i := range want {
 		if got[i] != want[i] {
 			t.Fatalf("got %v, want %v", got, want)
